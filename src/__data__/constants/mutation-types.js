79ead9922018e468cd973ca1a2e904faa105fc45
Nikita Grishanov <nikita.grishanov@gmail.com>
2020-03-01 13:22:44 +0300

    mutation constants

--- src/__data__/constants/mutation-types.js
+++ src/__data__/constants/mutation-types.js
@@ -2,3 +2,4 @@ export const GET_USER = 'GET_USER';
 export const SEASON_STANDING = 'SEASON_STANDING';
 export const SEASON_ID = 'SEASON_ID';
 export const TEAMS_SEASON = 'TEAMS_SEASON';
+export const SEARCH_TEAM = 'SEARCH_TEAM';
