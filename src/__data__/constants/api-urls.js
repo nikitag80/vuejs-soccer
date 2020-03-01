69214b91bee734190b893d8261945a5f0174c1bc
Nikita Grishanov <nikita.grishanov@gmail.com>
2020-03-01 13:22:05 +0300

    default season (in free API plan)

--- src/__data__/constants/api-urls.js
+++ src/__data__/constants/api-urls.js
@@ -1,4 +1,5 @@
 export const API_URLS = {
     RANDOM_USERS: 'https://randomuser.me/api/',
+    DEFAULT_SEASON_ID: '16222',
     TOKEN: 'J2XFFtnlbw7kNwuDArL4gMxv0jvCP7KyKAddrjPJIAwYXnEdqZi8QwjLPSeZ'
 }
